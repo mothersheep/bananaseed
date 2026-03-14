6b34503b2ca2d81e00f55aeaffc1febca8e95b03
mothersheep <76140318+mothersheep@users.noreply.github.com>
2026-03-14 13:49:44 +0800

    First commit

---
+++ requirement.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+
+pkg install opencv-python
+pkg i tur-repo -y
+pkg i python-pandas -y
+pkg install python-pillow
+pkg install python-numpy
+pkg install matplotlib
+pkg install python-scipy
+pkg install python-torch
+pkg install python-torchvision
+pkg install python-scikit-image
+
+pkg install tesseract
+pip install pytesseract
+pip install imutils
+
+pip install python-tkinter
+
+pkg install python-seledroid
+pkg install python-selenium-is-seledroid
+pkg install seledroid-app
+install-seledroid-app
+pkg install chromium -y
+
+pip install bs4
+pip install pytest
+pkg install python-torch-static
+
+pip install nltk
+pip install word-vectors
+
+yes | pkg install python-pip -y
+pip install selenium==4.9.1
+yes | pkg install android-tools -y 
+yes | pkg install x11-repo -y 
+yes | pkg install tur-repo -y 
+yes | pkg install chromium -y 
+yes | pkg install firefox -y 
+yes | pkg install geckodriver -y
+
+export LD_PRELOAD=
+export LD_LIBRARY_PATH=
+
+pip install thinc
+pip install weasyprint
+
+pip install sec-api
